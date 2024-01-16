c23b27ee80823c88bdc24a9e9977fcbe77ce9e7f
Roksana Zalesskaia <67091333+Roksikod@users.noreply.github.com>
2024-01-16 18:24:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     static boolean isGuessed = false;
 
